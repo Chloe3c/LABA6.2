fcb8065251cf6cd2004522d65c8dd9166b5640c6
Chloe3c <kristianishenko21@gmail.com>
2024-12-24 20:54:50 +0200

    Добавьте файлы проекта.

---
+++ classlib/Class1.cs
@@ -0,0 +1,26 @@
+﻿namespace classlib
+{
+    public class Class1
+    {
+        public static double[] GetMaxElements(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            double[] maxElements = new double[rows];
+
+            for (int i = 0; i < rows; i++)
+            {
+                double max = matrix[i, 0];
+                for (int j = 1; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] > max)
+                    {
+                        max = matrix[i, j];
+                    }
+                }
+                maxElements[i] = max;
+            }
+
+            return maxElements;
+        }
+    }
+}
---
+++ css/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using classlib;
+
+class Program
+{
+    static void Main()
+    {
+        double[,] matrix = new double[6, 4];
+
+        Console.WriteLine("Введите элементы матрицы 6x4 (вещественные числа):");
+        for (int i = 0; i < 6; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                Console.Write($"Элемент [{i + 1}, {j + 1}]: ");
+                matrix[i, j] = Convert.ToDouble(Console.ReadLine());
+            }
+        }
+
+        double[] maxElements = Class1.GetMaxElements(matrix);
+
+        Console.WriteLine("Максимальные элементы строк:");
+        for (int i = 0; i < maxElements.Length; i++)
+        {
+            Console.WriteLine($"Строка {i + 1}: {maxElements[i]}");
+        }
+    }
+}
